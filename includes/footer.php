5e00b1ec891a89d5feabb22cc623af343d111074
Tharindu Geeshan <mastermaxx2002@gmail.com>
2024-06-26 18:17:28 +0530

    added a new footer and OrderComplete

--- includes/footer.php
+++ includes/footer.php
@@ -14,6 +14,52 @@
 <br>
 </body>
 
-<?php include 'includes/footer.html'; ?> </body>
+<section class="bg-dark text-white py-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+        <h5>Company</h5>
+        <ul class="list-unstyled">
+          <li>Artisan Alley</li>
+          <li>Colombo 07</li>
+          <li>Phone: 0789989158</li>
+          <li>Email: ArtisanAlley@gmail.com</li>
+        </ul>
+      </div>
+      <div class="col-md-3">
+        <h5>Navigation</h5>
+        <ul class="list-unstyled">
+          <li><a href="Home.php" class="text-white">Home</a></li>
+          <li><a href="#" class="text-white">About Us</a></li>
+          <li><a href="#" class="text-white">Contact</a></li>
+          <li><a href="#" class="text-white">FAQs</a></li>
+          <li><a href="#" class="text-white">Terms & Conditions</a></li>
+        </ul>
+      </div>
+      <div class="col-md-3">
+        <h5>Follow Us</h5>
+        <ul class="list-unstyled d-flex">
+          <li class="me-3"><a href="#" class="text-white"><i class="fab fa-facebook-f"></i></a></li>
+          <li class="me-3"><a href="#" class="text-white"><i class="fab fa-twitter"></i></a></li>
+          <li class="me-3"><a href="#" class="text-white"><i class="fab fa-linkedin-in"></i></a></li>
+        </ul>
+      </div>
+      <div class="col-md-3">
+        <h5>Subscribe</h5>
+        <form>
+          <div class="input-group mb-3">
+            <input type="email" class="form-control" placeholder="Enter email" aria-label="Enter email" aria-describedby="button-addon2">
+            <button class="btn btn-primary" type="button" id="button-addon2">Subscribe</button>
+          </div>
+        </form>
+      </div>
+    </div>
+    <div class="row mt-4">
+      <div class="col text-center">
+        <p>&copy; 2022 Kelectrix Design. All rights reserved.</p>
+      </div>
+    </div>
+  </div>
+</section>
 
 </html>
